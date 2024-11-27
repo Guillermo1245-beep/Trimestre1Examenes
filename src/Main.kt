f1c8d0ee47c4982ddf52c5cb3e487939cefd7019
Cassandra1245 <guillermogarciasuarez@gmail.com>
2024-11-27 18:42:12 +0100

    a

--- src/Main.kt
+++ src/Main.kt
@@ -5,79 +5,72 @@ class Main {
 
 }
 
-fun Carrera (Estrategia: Array<String>, Circuito: String ) {
+fun Matrices (lista: MutableList<MutableList<String>>): String{
 
-    val circuito: List<String> = Circuito.split("")
-    var respuestaGrafica: MutableList<String> = mutableListOf()
-    var cagada: MutableList<String> = mutableListOf()
+    return "hola" +
+            ""
+    var MatrizAlineada: MutableList<String> = mutableListOf()
+    var ganador: String = "No gano ningo, vaya pringaos"
 
-    var suficiente: Int = 0
-    var respuesta: Boolean = true
-    var opcion: Int = 0
-
-    for (obstaculo in circuito){
-
-        if (opcion < Estrategia.size  && Estrategia[opcion] == "correr") {
-
-            if (obstaculo == "|") {
-                respuestaGrafica.add("/")
-                respuesta = false
-            } else if (obstaculo == "_") {
-                respuestaGrafica.add("_")
-                if (suficiente == 0) {
-                    respuesta = true
-                    suficiente++
-                }
-            } else {
-                cagada.add("?")
-            }
-
-        } else if (opcion < Estrategia.size &&  Estrategia[opcion] == "saltar") {
-
-            if (obstaculo == "|") {
-                respuestaGrafica.add("|")
-                if (suficiente == 0) {
-                    respuesta = true
-                    suficiente++
-                }
-            } else if (obstaculo == "_") {
-                respuestaGrafica.add("x")
-                if (suficiente == 0) {
-                    respuesta = true
-                    suficiente++
-                }
-            } else {
-                cagada.add("?")
-            }
+    for (Linea in lista) {
+        for (valor in Linea) {
+            MatrizAlineada.add(valor)
         }
-        opcion++
     }
 
-   if (respuesta) {
-       println("$respuestaGrafica$cagada true")
-   } else if (respuesta == false) {
-       println("$respuestaGrafica$cagada false")
-   }
+    // revisar verticales
+    for ( x in 0 .. 2) {
+        if (MatrizAlineada[x] == "X" && MatrizAlineada[x + 3] == "X" && MatrizAlineada[x + 3] == "X") {
+            ganador = "X"
+        } else if (MatrizAlineada[x] == "O" && MatrizAlineada[x + 3] == "O" && MatrizAlineada[x + 3] == "O")
+            ganador= "O"
+    }
+    // revisar horizontales
+    for ( x in 1 .. 3) {
+        if (MatrizAlineada[x] == "X" && MatrizAlineada[x + 1] == "X" && MatrizAlineada[x + 2] == "X") {
+            ganador ="X"
+        } else if (MatrizAlineada[x] == "O" && MatrizAlineada[x + 1] == "O" && MatrizAlineada[x + 2] == "O")
+            ganador= "O"
+    }
+
+    //revisar diagonales
+
+    if ((MatrizAlineada[0] == "X" && MatrizAlineada[4] == "X" && MatrizAlineada[8] == "X")) {
+        ganador = "X"
+    } else if (MatrizAlineada[0] == "O" && MatrizAlineada[4] == "O" && MatrizAlineada[8] == "O"){
+        ganador= "O"
+    }
+
+    if ((MatrizAlineada[2] == "X" && MatrizAlineada[4] == "X" && MatrizAlineada[6] == "X")) {
+        ganador = "X"
+    } else if (MatrizAlineada[2] == "O" && MatrizAlineada[4] == "O" && MatrizAlineada[6] == "O") {
+        ganador= "O"
+    }
+
 }
 
 fun main () {
 
-    val lista = arrayOf("correr", "saltar", "correr", "saltar", "correr")
-    Carrera( lista,"_|_|_")
-    val lista2 = arrayOf("correr", "correr", "correr", "saltar", "correr")
-    Carrera( lista2,"_|_|_")
-    val lista3 = arrayOf("correr", "correr", "saltar", "saltar", "correr")
-    Carrera( lista3,"_|_|_")
-    val lista4 = arrayOf("correr", "correr", "saltar", "saltar", "correr")
-    Carrera( lista4,"_|_|_|_")
-    val lista5 = arrayOf("correr", "saltar", "correr", "saltar")
-    Carrera( lista5,"_|_|_")
-    val lista6 = arrayOf("correr", "saltar", "correr", "saltar", "correr", "saltar", "correr")
-    Carrera( lista6,"_|_|_")
-    val lista7 = arrayOf("saltar", "saltar", "saltar", "saltar", "saltar")
-    Carrera( lista7,"|||||")
-    val lista8 = arrayOf("saltar", "saltar", "saltar", "saltar", "saltar")
-    Carrera( lista8,"||_||")
+    var linea1m1: MutableList<String> = mutableListOf("X", "O", "X")
+    var linea2m1: MutableList<String> = mutableListOf("O", "X", "O")
+    var linea3m1: MutableList<String> = mutableListOf("O", "O", "X")
+    var Matriz1 : MutableList<MutableList<String>> = mutableListOf(linea1m1,linea2m1,linea3m1)
+    var linea1m2: MutableList<String> = mutableListOf("", "O", "X")
+    var linea2m2: MutableList<String> = mutableListOf("", "X", "O")
+    var linea3m2: MutableList<String> = mutableListOf("", "O", "X")
+    var Matriz2 : MutableList<MutableList<String>> = mutableListOf(linea1m2,linea2m2,linea3m2)
+    var linea1m3: MutableList<String> = mutableListOf("O", "O", "O")
+    var linea2m3: MutableList<String> = mutableListOf("O", "X", "X")
+    var linea3m3: MutableList<String> = mutableListOf("O", "X", "X")
+    var Matriz3 : MutableList<MutableList<String>> = mutableListOf(linea1m3,linea2m3,linea3m3)
+    var linea1m4: MutableList<String> = mutableListOf("X", "O", "X")
+    var linea2m4: MutableList<String> = mutableListOf("X", "X", "O")
+    var linea3m4: MutableList<String> = mutableListOf("X", "X", "X")
+    var Matriz4 : MutableList<MutableList<String>> = mutableListOf(linea1m4,linea2m4,linea3m4)
 
+    Matrices(Matriz1)
+    Matrices(Matriz2)
+    Matrices(Matriz3)
+    Matrices(Matriz4)
 
 }
