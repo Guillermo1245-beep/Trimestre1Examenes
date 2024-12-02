ecfa6375b207ca10796337f5d64e4d1cea4c9898
Cassandra1245 <guillermogarciasuarez@gmail.com>
2024-12-02 17:41:17 +0100

    a

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,4 @@
-class Main {
-
-
-
-
-}
-
-fun comprobacionMatematica () {
-
+fun comprobacionMatematica() {
     var comprobacion1: String = "{a + b [c] * (2x2)}}}}"
     var comprobacion2: String = "{ [ a * ( c + d ) ] - 5 }"
     var comprobacion3: String = "{ a * ( c + d ) ] - 5 }"
@@ -25,64 +17,42 @@ fun comprobacionMatematica () {
 
     var operaciones: MutableList<List<String>> = mutableListOf(frase1, frase2, frase3, frase4, frase5, frase6, frase7)
 
-
     for (operacion in operaciones) {
         var ListraFiltrada: MutableList<String> = mutableListOf()
+        var funciona: Boolean = true
+
         for (simbolo in operacion) {
 
             if (simbolo == "(" || simbolo == "{" || simbolo == "[") {
                 ListraFiltrada.add(simbolo)
             }
 
-            if (ListraFiltrada.size > 0 && (simbolo == ")" || simbolo == "}" || simbolo == "]")) {
+            if (simbolo == ")" || simbolo == "}" || simbolo == "]") {
+                if (ListraFiltrada.isEmpty()) {
+                    funciona = false
+                    break
+                }
+
                 if (ListraFiltrada.last() == "(" && simbolo == ")") {
                     ListraFiltrada.removeAt(ListraFiltrada.size - 1)
-
-                } else if (ListraFiltrada.last() == "{" && simbolo == "}"){
+                } else if (ListraFiltrada.last() == "{" && simbolo == "}") {
                     ListraFiltrada.removeAt(ListraFiltrada.size - 1)
-
                 } else if (ListraFiltrada.last() == "[" && simbolo == "]") {
                     ListraFiltrada.removeAt(ListraFiltrada.size - 1)
-
                 } else {
-                    break
+                    funciona = false
                 }
             }
         }
 
-        if (ListraFiltrada.size == 0) {
-            println("¿Esta balanceada? = true")
-            println(ListraFiltrada)
+        if (ListraFiltrada.size == 0 && funciona) {
+            println("¿Está balanceada? = true")
         } else {
-            println("¿Esta balanceada? = false")
-            println(ListraFiltrada)
+            println("¿Está balanceada? = false")
         }
     }
 }
 
-fun main () {
-
+fun main() {
     comprobacionMatematica()
-
 }
-
-/*
-C:\Users\guillermo.garcia\.jdks\openjdk-23\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.2.3\lib\idea_rt.jar=51309:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.2.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\guillermo.garcia\Documents\Trimestre1Examenes\out\production\ExamenesTrimestre1;C:\Users\guillermo.garcia\.m2\repository\org\jetbrains\kotlin\kotlin-stdlib\2.0.20\kotlin-stdlib-2.0.20.jar;C:\Users\guillermo.garcia\.m2\repository\org\jetbrains\annotations\13.0\annotations-13.0.jar MainKt
-Operacion Matematica incorrecta
-[{, [, ], (, ), }, }, }, }]
-Operacion Matematica incorrecta
-[{, [, (, ), ], }]
-Operacion Matematica incorrecta
-[{, (, ), ], }]
-Operacion Matematica incorrecta
-[{, (, (, (, ), }]
-Operacion Matematica incorrecta
-[{, ], (, ), (, ), [, }]
-Operacion Matematica incorrecta
-[{, {, {, {, {, {, (, }, }, }, }, }, }]
-Operacion Matematica incorrecta
-[(]
-
-Process finished with exit code 0
-
-*/
